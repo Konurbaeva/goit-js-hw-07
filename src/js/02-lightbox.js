df1a5cbad3671605b98bcb240e2d2a1951c58d69
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-23 17:15:44 +0100

    lightBox library and gallery added

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,3 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const lightBoxMarkup = item => {
+    const { description, original, preview } = item;
+  
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+};
+
+
+  
+const lightBoxGalleryInserted = galleryItems
+  .map(lightBoxMarkup)
+  .join('');
+
+  const handler = (event) => {
+    event.preventDefault();
+    //Инициализация библиотеки SimpleLightbox 
+    let gallery = new SimpleLightbox('.gallery a');
+    gallery.on('show.simplelightbox', function () {
+   
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); 
+});
+}
+  
+  
+galleryEl.insertAdjacentHTML('beforeend', lightBoxGalleryInserted);
+galleryEl.addEventListener('click', handler);
