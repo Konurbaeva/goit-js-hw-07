be522a7c005438959e87a553534c51dd8ac87815
Konurbaeva <konurbaevamadina@gmail.com>
2022-01-23 14:43:18 +0100

    init commit task 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = item => {
+  const { description, original, preview } = item;
+
+  return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+  `;
+};
+
+const galleryInserted = galleryItems
+  .map(galleryItemsMarkup)
+  .join('');
 
 
-const list = gallery
-console.log(galleryItems);
+const galleryHandler = (event) => {
+
+  event.preventDefault();
+
+  if(event.target.nodeName != 'IMG') return;
+
+  const originalUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${originalUrl}">`)
+
+instance.show()
+
+}
+
+
+
+galleryEl.insertAdjacentHTML('beforeend', galleryInserted);
+galleryEl.addEventListener('click', galleryHandler);
